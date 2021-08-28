4b705d68f3b010dd1e4b8c3030d8bb15a598e956
981120lyb <490877451@qq.com>
2021-08-28 14:35:50 +0800

    修改程序图标和名称

--- app/src/main/java/com/example/myweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/myweather/ChooseAreaFragment.java
@@ -2,7 +2,9 @@ package com.example.myweather;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
